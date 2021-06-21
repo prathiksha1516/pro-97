83e0412fb184d16e8683d345937420aa038b4d86
prathiksha1516 <71827736+prathiksha1516@users.noreply.github.com>
2021-06-21 21:21:58 +0530

    Add files via upload

---
+++ pro class.py
@@ -0,0 +1,16 @@
+def folderWords():
+
+    rat=input("enter the file name")
+    no=0
+    dog=open(rat)
+    for cat in dog:
+        words=cat.split()
+        no =no+len(words)
+    print("number of words")    
+    print(no)    
+
+
+countWords()    
+   
+    with open(class)as a:data_a -a.read()
+    with open(name) w as a: a . write(data_b)
